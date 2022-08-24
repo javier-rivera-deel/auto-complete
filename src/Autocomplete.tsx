2ad532ce01e3f9ac3179716f86f8e6ad963c28f8
Javier Rivera <arq.javierriver@gmail.com>
2022-08-24 14:28:47 +0200

    grouping event listeners

--- src/Autocomplete.tsx
+++ src/Autocomplete.tsx
@@ -68,6 +68,24 @@ export default function Autocomplete() {
     setShowResults(false);
   };
 
+  // Accessibility management
+  const onKeyDown = (e: { keyCode: number }) => {
+    if (e.keyCode === 13) {
+      setActiveSuggestion(0);
+      setShowResults(false);
+      setUserInput(filteredSuggestions[activeSuggestion]);
+    } else if (e.keyCode === 38) {
+      if (activeSuggestion === 0) return;
+      setActiveSuggestion(activeSuggestion - 1);
+    } else if (e.keyCode === 40) {
+      if (activeSuggestion - 1 === filteredSuggestions.length) return;
+      setActiveSuggestion(activeSuggestion + 1);
+    } else if (e.keyCode === 27) {
+      setShowResults(false);
+      setFilteredSuggestions([]);
+    }
+  };
+
   const suggestionList = () => {
     // Highlight matching text
     const getSuggestionName = (name: string) => {
@@ -101,24 +119,6 @@ export default function Autocomplete() {
           <p className="no-suggestion"> No suggestions avaialable.</p>
         </div>
       );
-    }
-  };
-
-  // Accessibility management
-  const onKeyDown = (e: { keyCode: number }) => {
-    if (e.keyCode === 13) {
-      setActiveSuggestion(0);
-      setShowResults(false);
-      setUserInput(filteredSuggestions[activeSuggestion]);
-    } else if (e.keyCode === 38) {
-      if (activeSuggestion === 0) return;
-      setActiveSuggestion(activeSuggestion - 1);
-    } else if (e.keyCode === 40) {
-      if (activeSuggestion - 1 === filteredSuggestions.length) return;
-      setActiveSuggestion(activeSuggestion + 1);
-    } else if (e.keyCode === 27) {
-      setShowResults(false);
-      setFilteredSuggestions([]);
     }
   };
 
